83564ded2b213b4057de1c00d63cb123d3f3e913
Your Name <you@example.com>
2023-05-16 13:41:49 +0100

    Revert "traduction"
    This reverts commit 354e51e042977a30c08f7b3cb81b310eaa447706.

--- js/myslider.js
+++ js/myslider.js
@@ -1,4 +1,9 @@
-const slide=(img,name,version,price)=>`<div class="cars-slider__item">
+const slide = ({
+  img,
+  name,
+  version,
+  price,
+}) => `<div class="cars-slider__item">
 <div class="om-container">
     <div class="cars-slider__item-inner om-container__inner">
         <div class="cars-slider__item-description">
@@ -6,8 +11,8 @@ const slide=(img,name,version,price)=>`<div class="cars-slider__item">
             <h3 class="cars-slider__model"><span class="cars-slider__model-inner">${name} <span
                         class="cars-slider__model-br"><br></span>${version}</span></h3>
             <div class="cars-slider__item-description-sep"></div>
-            <div class="cars-slider__item-price"><span class="cars-slider__item-price-value"><span
-                        class="omcr-price-currency">$</span>${price}</span><span
+            <div class="cars-slider__item-price"><span class="cars-slider__item-price-value">${price}<span
+            class="omcr-price-currency">DH</span></span><span
                     class="cars-slider__item-price-period"> day</span></div>
             <div class="cars-slider__item-reserve">
                 <span class="cars-slider__item-reserve-button" data-car-id="15">Reserve Now</span>
@@ -43,6 +48,38 @@ const slide=(img,name,version,price)=>`<div class="cars-slider__item">
         </div>
     </div>
 </div>
-</div>`
+</div>`;
 
-const data=[]
+const data = [
+  {
+    img: './cars/Citroën-C3.png',
+    name: 'Citroën',
+    version: 'C3',
+    price: '500',
+  },
+  {
+    img: './cars/clio-4.png',
+    name: 'Renault',
+    version: ' CLIO 4',
+    price: '300',
+  },
+  {
+    img: './cars/dacia-logan.png',
+    name: 'Dacia',
+    version: 'Logan',
+    price: '200',
+  },
+  { img: './cars/fiat 500.png', name: 'Fiat', version: '500', price: '350' },
+  { img: './cars/golf 7.png', name: 'Golf', version: '7', price: '400' },
+  {
+    img: './cars/car2.png',
+    name: 'Range-Rover',
+    version: 'Evoque',
+    price: '700',
+  },
+];
+
+let slider_html = '';
+data.map((c) => (slider_html += slide(c)));
+console.log(slider_html);
+document.getElementById('myslider').innerHTML = slider_html;
